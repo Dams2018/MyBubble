000e23554474613041597b835dc46c2cd52515df
marco <marcoastorga.g@gmail.com>
2019-10-01 14:22:15 -0300

    update base

--- app/usuario/admin.py
+++ app/usuario/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 # importando base de datos
-from app.usuario.models import Usuario, Tipo_Usuario
+from app.usuario.models import Usuario, Tipo_Usuario, Avatar, Comentario, Curso
 # Register your models here.
 
 
 admin.site.register(Usuario)
 admin.site.register(Tipo_Usuario)
+admin.site.register(Avatar)
+admin.site.register(Comentario)
+admin.site.register(Curso)
 
--- app/usuario/models.py
+++ app/usuario/models.py
@@ -1,17 +1,37 @@
 from django.db import models
 
 # Create your models here.
+class Curso(models.Model):
+    idCurso=models.AutoField(primary_key=True)
+    nombreCurso=models.CharField(max_length=30)
+    activo=models.BooleanField(default=True)
+    
 class Tipo_Usuario(models.Model):
-    idTipoUsuario = models.CharField(max_length=10, primary_key = True)
+    idTipoUsuario = models.AutoField(primary_key = True)
     descripcion = models.CharField(max_length=10) 
     
+class Avatar(models.Model):
+    idAvatar=models.AutoField(primary_key=True)
+    imagen=models.CharField(max_length=50)
+    puntosAvatar=models.IntegerField(default=0)
+
 class Usuario(models.Model):
-    idUsuario=models.CharField(max_length=12, primary_key=True)
+    idUsuario=models.AutoField(primary_key=True)
+    run=models.CharField(max_length=12)
     nombre = models.CharField(max_length=25)
     apaterno = models.CharField(max_length=25)
     apmaterno = models.CharField(max_length=25)
     correo = models.EmailField(max_length=50)
     activo = models.BooleanField(default = True)
     puntos = models.IntegerField(default = 0)
-    idTipoUsuario = models.ForeignKey(Tipo_Usuario, null=True, blank = True, on_delete = models.DO_NOTHING)
+    idTipoUsuario = models.ForeignKey(Tipo_Usuario, null = True, blank = True, on_delete = models.DO_NOTHING)
+    idAvatar = models.ForeignKey(Avatar, null = True, Blank = True, on_delete = models.DO_NOTHING)
+    idCurso = models.ForeignKey(Curso, null = True, Blank = True, on_delete = models.DO_NOTHING)
+
+class Comentario(models.Model):
+    idComentario = models.AutoField(primary_key = True)
+    titulo = models.CharField(max_length= 20)
+    descripcion = models.CharField(max_length = 250)
+    idUsuario = models.ForeignKey(Usuario, null = True, Blank = True, on_delete = models.DO_NOTHING)
+
 
