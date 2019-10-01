953f40b5d84f6de31bc4af01a545d397e4aa98e7
marco <marcoastorga.g@gmail.com>
2019-10-01 13:32:29 -0300

    update

--- app/usuario/admin.py
+++ app/usuario/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+# importando base de datos
+from app.usuario.models import Usuario
 # Register your models here.
+
+
+admin.site.register(Usuario)
+
--- app/usuario/models.py
+++ app/usuario/models.py
@@ -2,5 +2,11 @@ from django.db import models
 
 # Create your models here.
 
-class Persona(models.Model):
-    rut=models.CharField(max_length=12, primary_key=True)
+class Usuario(models.Model):
+    idUsuario=models.CharField(max_length=12, primary_key=True)
+    nombre = models.CharField(max_length=25)
+    appaterno = models.CharField(max_length=25)
+    apmaterno = models.CharField(max_length=25)
+    correo = models.EmailField(max_length=50)
+    #activo = models.
+    #puntos = models
