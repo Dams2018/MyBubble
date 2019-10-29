f2d767a8b2cafd6a9b1f89717cafae2ddc662dae
marco <marcoastorga.g@gmail.com>
2019-10-29 01:07:06 -0300

    otro test

--- static/js/serviceworker.js
+++ static/js/serviceworker.js
@@ -1,16 +1,17 @@
+// Base Service Worker implementation.  To use your own Service Worker, set the PWA_SERVICE_WORKER_PATH variable in settings.py
+
 var staticCacheName = "django-pwa-v" + new Date().getTime();
-const OFFLINE_URL = 'offline.html';
 var filesToCache = [
-    '/offline.html',
+    '/offline',
     '/css/django-pwa-app.css',
-    '/images/icons/icon-72x72.png',
-    '/images/icons/icon-96x96.png',
-    '/images/icons/icon-128x128.png',
-    '/images/icons/icon-144x144.png',
-    '/images/icons/icon-152x152.png',
-    '/images/icons/icon-192x192.png',
-    '/images/icons/icon-384x384.png',
-    '/images/icons/icon-512x512.png',
+    '/static/images/icons/icon-72x72.png',
+    '/static/images/icons/icon-96x96.png',
+    '/static/images/icons/icon-128x128.png',
+    '/static/images/icons/icon-144x144.png',
+    '/static/images/icons/icon-152x152.png',
+    '/static/images/icons/icon-192x192.png',
+    '/static/images/icons/icon-384x384.png',
+    '/static/images/icons/icon-512x512.png',
     '/static/images/icons/splash-640x1136.png',
     '/static/images/icons/splash-750x1334.png',
     '/static/images/icons/splash-1242x2208.png',
